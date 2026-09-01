37bed984f70997338a42fd35263433478efa657c
fauza-alifa <fauzahralifa@gmail.com>
2026-09-01 08:51:58 +0700

    adding circle icon button widget

---
+++ lib/widgets/circle_icon_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+// circle icon button that will show on some screen
+class CircleIconButton extends StatelessWidget {
+  const CircleIconButton({
+    super.key,
+    required this.icon,
+    required this.onTap,
+    required this.iconColor,
+  });
+
+  final IconButton icon;
+  final VoidCallback onTap;
+  final Color iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      customBorder: CircleBorder(),
+      child: Container(
+        width: 42,
+        height: 42,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.12),
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Icon(icon, size: 20, color: iconColor),
+      ),
+    );
+  }
+}
